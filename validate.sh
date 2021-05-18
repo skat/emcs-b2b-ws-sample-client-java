5c1c2406b9eeaa8d2bb0dbdf1db5598af36bc056
cdcurry <cdcurry@krispoint.com>
2021-05-18 20:16:53 +0200

    Updated sample documents and added check of sample documents against schemas

---
+++ validate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+xmllint --noout --schema schema/ie705.xsd ie705.xml
+xmllint --noout --schema schema/ie815.xsd ie815.xml
+xmllint --noout --schema schema/ie818.xsd ie818.xml
+xmllint --noout --schema schema/ie810.xsd ie810.xml
+xmllint --noout --schema schema/ie813.xsd ie813.xml
+xmllint --noout --schema schema/ie819.xsd ie819.xml
+xmllint --noout --schema schema/ie825.xsd ie825.xml
+xmllint --noout --schema schema/ie837.xsd ie837.xml
+xmllint --noout --schema schema/ie871.xsd ie871.xml
+
+# Comment out to verify validation failure
+# xmllint --noout --schema schema/ie815.xsd sample/ie815-invalid.xml
