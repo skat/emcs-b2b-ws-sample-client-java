d7d596b46d2c9fa2fca898ccc6544f70439a138b
cdcurry <cdcurry@krispoint.com>
2023-02-06 10:56:33 +0100

    chore: Updated JavaDoc

--- src/test/java/dk/skat/emcs/b2b/sample/OIOPaamindelseSamlingHentClientTest.java
+++ src/test/java/dk/skat/emcs/b2b/sample/OIOPaamindelseSamlingHentClientTest.java
@@ -4,6 +4,23 @@ import org.junit.Test;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 
+/**
+ * OIOPaamindelseSamlingHent Test
+ *
+ * This client depends on the Consignee NOT submitting the required IE818 within the deadline from the time the
+ * Consignee received the IE810. This is currently 5 days as explained here: https://skat.dk/data.aspx?oid=2244647
+ *
+ * As such it is very not practical to do a programmatic test that sleeps for 5 days and then calls OIOPaamindelseSamlingHent.
+ *
+ * To test this OIOPaamindelseSamlingHent follow these steps:
+ *
+ * Step 1: Submit IE15 using {@link OIOLedsageDocumentOpretClientTest#invoke()} and do NOT submit the corresponding IE181
+ * Step 2: Wait 6 days (to be sure)
+ * Step 3: Invoke the OIOPaamindelseSamlingHent service (this test)
+ *
+ * @author SKAT
+ * @since 1.2
+ */
 public class OIOPaamindelseSamlingHentClientTest extends BaseClientTest {
 
     @Test
