020efe71c366bce409914b7bcb2a4b75899d1703
cdcurry <cdcurry@krispoint.com>
2025-05-01 08:46:53 +0200

    Improve OIOLedsageDokumentOmdirigeretAdvisSamlingHentClientTest

--- src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDokumentOmdirigeretAdvisSamlingHentClientTest.java
+++ src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDokumentOmdirigeretAdvisSamlingHentClientTest.java
@@ -1,11 +1,14 @@
 package dk.skat.emcs.b2b.sample;
 
+import oio.skat.emcs.ws._1_0.OIOLedsageDokumentOmdirigeretAdvisSamlingHentOType;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
 
 /**
  * OIOLedsageDokumentOmdirigeretAdvisSamlingHent Test (IE803 as response)
@@ -29,8 +32,9 @@ public class OIOLedsageDokumentOmdirigeretAdvisSamlingHentClientTest extends Bas
             String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
             String afgiftOperatoerPunktAfgiftIdentifikator = "DK31175143300";
             OIOLedsageDokumentOmdirigeretAdvisSamlingHentClient client = new OIOLedsageDokumentOmdirigeretAdvisSamlingHentClient(getEndpoint(OIO_LEDSAGE_DOKUMENT_OMDIRIGERET_ADVIS_SAMLING_HENT));
-            client.invoke(virksomhedSENummerIdentifikator,
+            OIOLedsageDokumentOmdirigeretAdvisSamlingHentOType response = client.invoke(virksomhedSENummerIdentifikator,
                     afgiftOperatoerPunktAfgiftIdentifikator);
+            assertFalse(hasError(response.getHovedOplysningerSvar()));
         }
     }
 }
