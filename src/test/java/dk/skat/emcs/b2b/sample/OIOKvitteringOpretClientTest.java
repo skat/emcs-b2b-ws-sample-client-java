dd2437ca6c39f2d42fea127311d3c8a371900b42
cdcurry <cdcurry@krispoint.com>
2025-09-19 15:49:10 +0200

    Simplify

--- src/test/java/dk/skat/emcs/b2b/sample/OIOKvitteringOpretClientTest.java
+++ src/test/java/dk/skat/emcs/b2b/sample/OIOKvitteringOpretClientTest.java
@@ -1,10 +1,15 @@
 package dk.skat.emcs.b2b.sample;
 
 import org.junit.Test;
+
 import java.io.File;
+
+import static org.junit.Assume.assumeNotNull;
 
 /**
  * OIOKvitteringOpretClient Test
+ *
+ * IMPORTANT: OIOLedsageDokumentSamlingHent is also tested as part of {@link OIOKvitteringSamlingHentClientTest#testScenario()}}
  *
  * @author SKAT
  * @since 1.2
@@ -13,19 +18,13 @@ public class OIOKvitteringOpretClientTest extends BaseClientTest {
 
     @Test
     public void invoke() throws Exception {
-        String endpointURL =
-                getEndpoint("OIOKvitteringOpret");
-        if (endpointURL != null) {
-            File ie818 = new File ("ie818.xml");
-            // this CVR number present in the certificate.
-            String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
-
-            OIOKvitteringOpretClient oioKvitteringOpretClient = new OIOKvitteringOpretClient(endpointURL);
-            oioKvitteringOpretClient.invoke(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator,ie818);
-        }
+        assumeNotNull(getEndpoint(OIO_KVITTERING_OPRET));
+        File ie818 = new File("ie818.xml");
+        String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
+        String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
+        OIOKvitteringOpretClient oioKvitteringOpretClient = new OIOKvitteringOpretClient(getEndpoint(OIO_KVITTERING_OPRET));
+        oioKvitteringOpretClient.invoke(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ie818);
     }
 
 }
