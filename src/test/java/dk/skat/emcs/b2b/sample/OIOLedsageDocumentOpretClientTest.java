61bec34b7af1648a9bad9f93d98ffbf293406640
cdcurry <cdcurry@krispoint.com>
2025-09-19 20:42:04 +0200

    Simplify

--- src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDocumentOpretClientTest.java
+++ src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDocumentOpretClientTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 import java.io.File;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeNotNull;
 
 /**
  * OIOLedsageDocumentOpretClient Test
@@ -15,13 +17,11 @@ import static org.junit.Assert.assertTrue;
  */
 public class OIOLedsageDocumentOpretClientTest extends BaseClientTest {
 
-    private String endpointURL = getEndpoint("OIOLedsageDocumentOpret");
-
     /**
      * Test submit of ie815 - no delegation
-     *
+     * <p>
      * Expected Console Output (sample):
-     *
+     * <p>
      * *******************************************************************
      * ** HovedOplysningerSvar
      * **** Transaction Id: e0372c6a-8ba6-4d1e-ae63-d813c2df851c
@@ -34,43 +34,33 @@ public class OIOLedsageDocumentOpretClientTest extends BaseClientTest {
      */
     @Test
     public void invoke() throws Exception {
-        if (endpointURL != null) {
-            // Path to where the IE815 document is located
-            File ie815 = new File("ie815.xml");
-            // VAT Number of the entity sending. Rule of thumb: this number matches
-            // this CVR number present in the certificate.
-            String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
-
-            OIOLedsageDokumentOpretClient oioLedsageDocumentClient = new OIOLedsageDokumentOpretClient(endpointURL);
-            oioLedsageDocumentClient.invoke(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator, ie815);
-        }
+        assumeNotNull(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        File ie815 = new File("ie815.xml");
+        String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
+        String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
+        OIOLedsageDokumentOpretClient client = new OIOLedsageDokumentOpretClient(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        OIOLedsageDokumentOpretOType response =  client.invoke2(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ie815);
+        assertNotNull("Did not receive ARC number.", response.getOutput().getLedsageDokument().getLedsagedokumentARCIdentifikator());
     }
 
     @Test
     public void invoke19552101() throws Exception {
-        if (endpointURL != null) {
-            // Path to where the IE815 document is located
-            File ie815 = new File("ie815-19552101.xml");
-            // VAT Number of the entity sending. Rule of thumb: this number matches
-            // this CVR number present in the certificate.
-            String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = "DK19552101100";
-
-            OIOLedsageDokumentOpretClient oioLedsageDocumentClient = new OIOLedsageDokumentOpretClient(endpointURL);
-            oioLedsageDocumentClient.invoke(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator, ie815);
-        }
+        assumeNotNull(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        File ie815 = new File("ie815-19552101.xml");
+        String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
+        String afgiftOperatoerPunktAfgiftIdentifikator = "DK19552101100";
+        OIOLedsageDokumentOpretClient oioLedsageDocumentClient = new OIOLedsageDokumentOpretClient(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        OIOLedsageDokumentOpretOType response = oioLedsageDocumentClient.invoke2(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ie815);
+        assertTrue(hasError(response.getHovedOplysningerSvar(), 302));
     }
 
     /**
      * Test submit of ie815 - valid delegation
-     *
+     * <p>
      * Expected Console Output (sample):
-     *
+     * <p>
      * *******************************************************************
      * ** HovedOplysningerSvar
      * **** Transaction Id: ad7ca908-6ed7-4b3b-994f-20b3beed6af2
@@ -83,93 +73,81 @@ public class OIOLedsageDocumentOpretClientTest extends BaseClientTest {
      */
     @Test
     public void invokeValidDelegation() throws Exception {
-        if (endpointURL != null) {
-            // Path to where the IE815 document is located
-            File ie815 = new File("ie815-valid-delegation.xml");
-            // VAT Number of the entity sending. Rule of thumb: this number matches
-            // this CVR number present in the certificate.
-            String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = "DK82065873300";
-
-            OIOLedsageDokumentOpretClient oioLedsageDocumentClient = new OIOLedsageDokumentOpretClient(endpointURL);
-            oioLedsageDocumentClient.invoke(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator, ie815);
-        }
+        assumeNotNull(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        // Path to where the IE815 document is located
+        File ie815 = new File("ie815-valid-delegation.xml");
+        // VAT Number of the entity sending. Rule of thumb: this number matches
+        // this CVR number present in the certificate.
+        String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
+        // Excise number
+        String afgiftOperatoerPunktAfgiftIdentifikator = "DK82065873300";
+        OIOLedsageDokumentOpretClient oioLedsageDocumentClient = new OIOLedsageDokumentOpretClient(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        OIOLedsageDokumentOpretOType response = oioLedsageDocumentClient.invoke2(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ie815);
+        assertNotNull("Did not receive ARC number.", response.getOutput().getLedsageDokument().getLedsagedokumentARCIdentifikator());
     }
 
     /**
      * Test submit of ie815 - CVR number is not parent to VAT number in VirksomhedSENummerIdentifikator
-     *
+     * <p>
      * Expected Console Output:
-     *
+     * <p>
      * Error code = 300
      * Error Text = SE nummeret 12345678 har ingen tilhoersforhold til det anvendte certifikat.
      */
     @Test
     public void invokeCertificateHasNoRelationToVirksomhedSENummerIdentifikator() throws Exception {
-        if (endpointURL != null) {
-            // Path to where the IE815 document is located
-            File ie815 = new File("ie815.xml");
-            // This SE does not match CVR of certificate.
-            String virksomhedSENummerIdentifikator = "12345678";
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
-
-            OIOLedsageDokumentOpretClient oioLedsageDocumentClient = new OIOLedsageDokumentOpretClient(endpointURL);
-            oioLedsageDocumentClient.invoke(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator, ie815);
-        }
+        assumeNotNull(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        // Path to where the IE815 document is located
+        File ie815 = new File("ie815.xml");
+        // This SE does not match CVR of certificate.
+        String virksomhedSENummerIdentifikator = "12345678";
+        // Excise number
+        String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
+        OIOLedsageDokumentOpretClient client = new OIOLedsageDokumentOpretClient(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        OIOLedsageDokumentOpretOType response = client.invoke2(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ie815);
+        assertTrue(hasError(response.getHovedOplysningerSvar(), 300));
     }
 
     /**
      * Test submit of ie815 - no delegation exists
-     *
+     * <p>
      * Expected Console Output:
-     *
+     * <p>
      * Error Code: 302
      * Error Text: Den angivet afgift operatoer har enten ikke tillades til at udfoere denne operation eller har ikke delegeret sine rettigheder til det firma hvis certifikat er anvendt i kaldet af webservicen
      */
     @Test
     public void invokeCertificateHasNoDelegation() throws Exception {
-        if (endpointURL != null) {
-            // Path to where the IE815 document is located
-            File ie815 = new File("ie815-no-delegation.xml");
-            // This SE does not match CVR of certificate.
-            String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = "DK12345678300";
-
-            OIOLedsageDokumentOpretClient client = new OIOLedsageDokumentOpretClient(endpointURL);
-            client.invoke(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator, ie815);
-        }
+        assumeNotNull(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        File ie815 = new File("ie815-no-delegation.xml");
+        String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
+        String afgiftOperatoerPunktAfgiftIdentifikator = "DK12345678300";
+        OIOLedsageDokumentOpretClient client = new OIOLedsageDokumentOpretClient(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        OIOLedsageDokumentOpretOType response = client.invoke2(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ie815);
+        assertTrue(hasError(response.getHovedOplysningerSvar(), 302));
     }
 
     /**
      * Trigger error 104
-     *
+     * <p>
      * Expected Console Output:
-     *
+     * <p>
      * Error Code: 104
      * Error Text: Den angivet afgift operatoer stemmer ikke overens med vareafsender (consignor) i IE815 beskeden
      */
     @Test
     public void invokeCertificatTriggerError104() throws Exception {
-        if (endpointURL != null) {
-            // Path to where the IE815 document is located
-            File ie815 = new File("ie815-trigger-error-104.xml");
-            // This SE does not match CVR of certificate.
-            String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
-
-            OIOLedsageDokumentOpretClient client = new OIOLedsageDokumentOpretClient(endpointURL);
-            OIOLedsageDokumentOpretOType response = client.invoke2(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator, ie815);
-            assertTrue(hasError(response.getHovedOplysningerSvar(), 104));
-        }
+        assumeNotNull(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        File ie815 = new File("ie815-trigger-error-104.xml");
+        String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
+        String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
+        OIOLedsageDokumentOpretClient client = new OIOLedsageDokumentOpretClient(getEndpoint(OIO_LEDSAGEDOCUMENT_OPRET));
+        OIOLedsageDokumentOpretOType response = client.invoke2(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ie815);
+        assertTrue(hasError(response.getHovedOplysningerSvar(), 104));
     }
-
 
 }
