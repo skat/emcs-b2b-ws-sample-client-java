966f059af98fc3ea2eec653f6e7aa75e1b4cfd3e
cdcurry <cdcurry@krispoint.com>
2025-09-19 14:05:33 +0200

    Simplify

--- src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDokumentSamlingHentClientTest.java
+++ src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDokumentSamlingHentClientTest.java
@@ -4,6 +4,7 @@ import oio.skat.emcs.ws._1_0.OIOLedsageDokumentSamlingHentOType;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assume.assumeNotNull;
 
 /**
  * OIOLedsageDokumentSamlingHent Test
@@ -15,26 +16,18 @@ import static org.junit.Assert.assertFalse;
  */
 public class OIOLedsageDokumentSamlingHentClientTest extends BaseClientTest {
 
+    // This tests assumes knowledge of existing ARC.
     @Test
     public void invoke() throws Exception {
-        String endpointURL =
-                getEndpoint("OIOLedsageDokumentSamlingHent");
-
-        if (endpointURL != null) {
-
-            // The ARC number
-            String ARCnumber = getConfig().getString("dk.skat.emcs.b2b.sample.ARC");
-            // VAT Number of the entity sending. Rule of thumb: this number matches
-            // this CVR number present in the certificate.
-            String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-            // Excise number
-            String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
-
-            OIOLedsageDokumentSamlingHentClient client = new OIOLedsageDokumentSamlingHentClient(endpointURL);
-            OIOLedsageDokumentSamlingHentOType response = client.invoke(virksomhedSENummerIdentifikator,
-                    afgiftOperatoerPunktAfgiftIdentifikator, ARCnumber);
-            assertFalse(hasError(response.getHovedOplysningerSvar()));
-        }
+        assumeNotNull(getEndpoint(OIO_LEDSAGE_DOKUMENT_SAMLLING_HENT));
+        // The ARC number
+        String ARCnumber = getConfig().getString("dk.skat.emcs.b2b.sample.ARC");
+        String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
+        String afgiftOperatoerPunktAfgiftIdentifikator = getAfgiftOperatoerPunktAfgiftIdentifikator();
+        OIOLedsageDokumentSamlingHentClient client = new OIOLedsageDokumentSamlingHentClient(getEndpoint(OIO_LEDSAGE_DOKUMENT_SAMLLING_HENT));
+        OIOLedsageDokumentSamlingHentOType response = client.invoke(virksomhedSENummerIdentifikator,
+                afgiftOperatoerPunktAfgiftIdentifikator, ARCnumber);
+        assertFalse(hasError(response.getHovedOplysningerSvar()));
     }
 
 }
