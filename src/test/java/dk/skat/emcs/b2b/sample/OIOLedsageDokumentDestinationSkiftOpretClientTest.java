a8946813b8a6860ed3ed98acfb8835049ddf9ea7
cdcurry <cdcurry@krispoint.com>
2025-09-22 07:54:03 +0200

    Cleanup and add asserts

--- src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDokumentDestinationSkiftOpretClientTest.java
+++ src/test/java/dk/skat/emcs/b2b/sample/OIOLedsageDokumentDestinationSkiftOpretClientTest.java
@@ -1,13 +1,15 @@
 package dk.skat.emcs.b2b.sample;
 
+import oio.skat.emcs.ws._1_0.OIOLedsageDokumentDestinationSkiftOpretOType;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assume.assumeNotNull;
 
 /**
  * OIOLedsageDokumentDestinationSkiftOpret Test
  *
- * IMPORTANT: OIOLedsageDokumentDestinationSkiftOpret is also tested as part of {@link OIOLedsageDokumentDestinationSkiftSamlingHentClientTest#invoke()}
+ * NOTE: OIOLedsageDokumentDestinationSkiftOpret is also tested as part of {@link OIOLedsageDokumentDestinationSkiftSamlingHentClientTest#invoke()}
  *
  * @author SKAT
  * @since 1.2
@@ -19,12 +21,12 @@ public class OIOLedsageDokumentDestinationSkiftOpretClientTest extends BaseClien
         assumeNotNull(getEndpoint(OIO_LEDSAGE_DOKUMENT_DESTINATION_SKIFT_OPRET));
         String ie813 = "ie813.xml";
         String virksomhedSENummerIdentifikator = getVirksomhedSENummerIdentifikator();
-        // Excise number
         String afgiftOperatoerPunktAfgiftIdentifikator = "DK82065873300";
         String arc = "11DKJKA05CB5I1EXW2KL9";
         OIOLedsageDokumentDestinationSkiftOpretClient client = new OIOLedsageDokumentDestinationSkiftOpretClient(getEndpoint(OIO_LEDSAGE_DOKUMENT_DESTINATION_SKIFT_OPRET));
-        client.invoke(virksomhedSENummerIdentifikator,
+        OIOLedsageDokumentDestinationSkiftOpretOType response = client.invoke(virksomhedSENummerIdentifikator,
                 afgiftOperatoerPunktAfgiftIdentifikator, ie813, arc);
+        assertFalse(hasError(response.getHovedOplysningerSvar()));
     }
 
 }
